1dec67248ca848c5820f9f009992a88e6ee07c28
Hamim <hamimc232@gmail.com>
2024-08-24 21:51:57 -0400

    Code formatting

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,7 +1,7 @@
-import { NextResponse } from 'next/server'
-import { Pinecone } from '@pinecone-database/pinecone'
-import OpenAI from 'openai'
-import { Stardos_Stencil } from 'next/font/google'
+import { NextResponse } from "next/server";
+import { Pinecone } from "@pinecone-database/pinecone";
+import OpenAI from "openai";
+import { Stardos_Stencil } from "next/font/google";
 
 const systemPrompt = `
 System Prompt:
@@ -55,71 +55,71 @@ Top 3 Professors:
    - Subject: Biology
    - Rating: 4.0/5
    - Summary: Dr. Allen's lectures are well-organized and she is very fair with grading. While her courses are challenging, she provides ample support for students to succeed.
-`
+`;
 
 export async function POST(req) {
-    const data = await req.json()
-    const pc = new Pinecone({
-        apiKey: process.env.PINECONE_API_KEY,
-    })
-    const index = pc.index('rag').namespace('ns1')
-    const openai = new OpenAI()
+  const data = await req.json();
+  const pc = new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY,
+  });
+  const index = pc.index("rag").namespace("ns1");
+  const openai = new OpenAI();
 
-    const text = data[data.length - 1].content
-    const embedding = await openai.embeddings.create({
-        model: 'text-embedding-ada-002', // Updated model name for embeddings
-        input: text
-    })
+  const text = data[data.length - 1].content;
+  const embedding = await openai.embeddings.create({
+    model: "text-embedding-ada-002", // Updated model name for embeddings
+    input: text,
+  });
 
-    const results = await index.query({
-        topK: 3,
-        includeMetadata: true,
-        vector: embedding.data[0].embedding
-    })
+  const results = await index.query({
+    topK: 3,
+    includeMetadata: true,
+    vector: embedding.data[0].embedding,
+  });
 
-    let resultString = 
-    '\n\nReturned results from vector db (done automatically): '
-    results.matches.forEach((match) => {
-        resultString += `
+  let resultString =
+    "\n\nReturned results from vector db (done automatically): ";
+  results.matches.forEach((match) => {
+    resultString += `
         Professor: ${match.id}
         Review: ${match.metadata.stars}
         Subject: ${match.metadata.subject}
         Stars: ${match.metadata.stars}
         \n\n
-        `
-    })
+        `;
+  });
 
-    const lastMessage = data[data.length - 1]
-    const lastMessageContent = lastMessage.content + resultString
-    const lastDataWithoutLastMessage = data.slice(0, data.length - 1)
-    const completion = await openai.chat.completions.create({
-        message:[
-            {role: 'system', content: systemPrompt},
-            ...lastDataWithoutLastMessage,
-            {role: 'user', content: lastMessageContent}
-        ],
-        model: 'gpt-4o-mini',
-        stream: true,
-    })
+  const lastMessage = data[data.length - 1];
+  const lastMessageContent = lastMessage.content + resultString;
+  const lastDataWithoutLastMessage = data.slice(0, data.length - 1);
+  const completion = await openai.chat.completions.create({
+    message: [
+      { role: "system", content: systemPrompt },
+      ...lastDataWithoutLastMessage,
+      { role: "user", content: lastMessageContent },
+    ],
+    model: "gpt-4o-mini",
+    stream: true,
+  });
 
-    const stream = ReadableStream({
-        async start(controller){
-            const encoder = new TextEncoder()
-            try{
-                for await (const chunk of completion){
-                    const content = chunk.choices[0]?.delta?.content
-                    if(content){
-                        const text= encoder.encode(content)
-                        controller.enqueue(text)
-                    }
-                }
-            } catch (err){
-                controller.error(err)
-            } finally {
-                controller.close()
-            }
-        },
-    })
+  const stream = ReadableStream({
+    async start(controller) {
+      const encoder = new TextEncoder();
+      try {
+        for await (const chunk of completion) {
+          const content = chunk.choices[0]?.delta?.content;
+          if (content) {
+            const text = encoder.encode(content);
+            controller.enqueue(text);
+          }
+        }
+      } catch (err) {
+        controller.error(err);
+      } finally {
+        controller.close();
+      }
+    },
+  });
 
-    return new NextResponse(stream)
+  return new NextResponse(stream);
 }
--- app/page.js
+++ app/page.js
@@ -1,88 +1,90 @@
-'use client'
+"use client";
 import { Box } from "@mui/material";
 import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
-    const [messages, setMessages] = useState([
-      {
+  const [messages, setMessages] = useState([
+    {
       role: "assistant",
-      content: "Hi! I'm the Rate My Professor support assistant. How can I help you today?"
-      }
-    ])
-const [message, setMessage] = useState('')
-const sendMessage = async ()=>{
-  setMessages((messages)=>[
-    ...messages,
-    {role: "user", content: ''},
-    {role: "assistant", content: ''}
-  ])
-
-  setMessage('')
-  const response = fetch('/api/chat',{
-    method: "POST",
-    headers: {
-      'Content-Type': 'application/json'
+      content:
+        "Hi! I'm the Rate My Professor support assistant. How can I help you today?",
     },
-    body: JSON.stringify([...messages,{role: 'user', content: message}]),
-  }).then(async(res)=>{
-    const reader = res.body.getReader()
-    const decocder = new TextDecoder()
-    let result = ''
-    return reader.read().then(function processText({done, value}){
-      if(done){
-        return result
-      }
-      const text = decoder.decode(value || new Uint8Array(),{stream: true})
-      setMessages((messages)=>{
-        let lastMessage = messages[messages.lenght -1]
-        let otherMessages = messages.slice(0, messages.length -1)
-        return [
-          ...otherMessages,
-          {...lastMessages, content: lastMessages.content + text},
-        ]
-      })
+  ]);
+  const [message, setMessage] = useState("");
+  const sendMessage = async () => {
+    setMessages((messages) => [
+      ...messages,
+      { role: "user", content: "" },
+      { role: "assistant", content: "" },
+    ]);
 
-      return reader.read().then(processText)
-    })
-  })
-  
-}
-return (
-  <Box
-  width="100vw"
-  height="100vh"
-  display="flex"
-  flexDirection="column"
-  justifyContent="center"
-  alignItems="center"
-  >
-    <Stack 
-    direction = "column" 
-    width = "500px" 
-    height = "100px" 
-    border="1px solid black" 
-    p={2} 
-    spacing={3}
+    setMessage("");
+    const response = fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify([...messages, { role: "user", content: message }]),
+    }).then(async (res) => {
+      const reader = res.body.getReader();
+      const decocder = new TextDecoder();
+      let result = "";
+      return reader.read().then(function processText({ done, value }) {
+        if (done) {
+          return result;
+        }
+        const text = decoder.decode(value || new Uint8Array(), {
+          stream: true,
+        });
+        setMessages((messages) => {
+          let lastMessage = messages[messages.lenght - 1];
+          let otherMessages = messages.slice(0, messages.length - 1);
+          return [
+            ...otherMessages,
+            { ...lastMessages, content: lastMessages.content + text },
+          ];
+        });
+
+        return reader.read().then(processText);
+      });
+    });
+  };
+  return (
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
     >
-      {
-        messages.map((message, index)=>{
-          ;<Box
-          key={index}
-          display="flex"
-          justifyContent={
-            message.role === 'assistant' ? 'flex-start' : 'flex-end'
-          }
-          ><Box 
-          bgcolor={
-            message.role === 'assistant' ? 'primary.main' : 'secondary.main'
-          }>
-            {message.content}
+      <Stack
+        direction="column"
+        width="500px"
+        height="100px"
+        border="1px solid black"
+        p={2}
+        spacing={3}
+      >
+        {messages.map((message, index) => {
+          <Box
+            key={index}
+            display="flex"
+            justifyContent={
+              message.role === "assistant" ? "flex-start" : "flex-end"
+            }
+          >
+            <Box
+              bgcolor={
+                message.role === "assistant" ? "primary.main" : "secondary.main"
+              }
+            >
+              {message.content}
             </Box>
-          </Box>
-        })
-      }
-    </Stack>
-  </Box>
-)
+          </Box>;
+        })}
+      </Stack>
+    </Box>
+  );
 }
